6480ec413a6c7d02623b2c97edf0457e9017ba54
Malte <mfj@uchicago.edu>
2017-07-27 22:31:08 -0500

    Moved definition of psi_so out of model class

--- model.py
+++ model.py
@@ -7,38 +7,40 @@ class Model(object):
             f=1.2e-4,
             b_s=0.025,
             kappa_const=6e-5,
-            H_max_so=2000,
             B_int=3e3,
-            psi_so_max=4.,
             nz=100,
             A=7e13,
             sol_init=None,
             kappa=None,
             dkappa_dz=None,
+            psi_so=None,
     ):
    
         self.f = f
         self.A = A
         self.zi=np.asarray(np.linspace(-1, 0, nz))
         if kappa and dkappa_dz:  
-            self.kappa= lambda z,H: kappa(z,H)/ (H**2 * f)
-            self.dkappa_dz=lambda z,H: dkappa_dz(z,H) / (H * f)
+            self.kappa= lambda z,H: kappa(z,H)/ (H**2 * self.f)
+            self.dkappa_dz=lambda z,H: dkappa_dz(z,H) / (H * self.f)
         elif kappa or dkappa_dz:
            raise ValueError('If you want kappa profile you need to provide both kappa and dkappa_dz')
         else:
-            self.kappa= lambda z,H: kappa_const/ (H**2 * f)
-            self.dkappa_dz=lambda z,H: 0    
+            self.kappa= lambda z,H: kappa_const/ (H**2 * self.f)
+            self.dkappa_dz=lambda z,H: 0   
+        if psi_so:
+            self.psi_so=lambda z,H: psi_so(z,H)/ (self.f * H**3)
+        else:
+            self.psi_so=lambda z,H: 0
+            
         self.b = -b_s / f**2
         self.B_int = B_int
-        self.psi_so_max = psi_so_max
-        self.H_max_so = H_max_so
         if sol_init:
             self.sol_init = sol_init
         else:
             sol_init = np.zeros((4, nz))
             sol_init[0,:] = np.ones((nz))
             sol_init[2,:] = -0.1 * np.ones((nz))
-            sol_init[3,:] = -self.bz(H_max_so + 200) * np.ones((nz))
+            sol_init[3,:] = -self.bz(2000.) * np.ones((nz))
             self.sol_init = sol_init
 
     def alpha(self, z, H):
@@ -50,16 +52,13 @@ class Model(object):
     def bc(self, ya, yb, p):
         return np.array([ya[0], yb[0], ya[1], ya[3] + self.bz(p[0]), yb[2] - self.b/p[0]])
 
-    def psi_so(self, z, H):
-        pre = self.psi_so_max * 1e6 / (self.f * H**3)
-        return pre * np.sin([-np.pi * max(H * x, -self.H_max_so) / self.H_max_so for x in z])**2
 
     def ode(self, z, y, p):
         H = p[0]
         return np.vstack((y[1], y[2], y[3], self.alpha(z, H) * y[3] * (y[0] - self.psi_so(z, H) - self.A * self.dkappa_dz(z, H)/(H**2))))
 
     def solve(self):
-        res = integrate.solve_bvp(self.ode, self.bc, self.zi, self.sol_init, p=[self.H_max_so - 200])
+        res = integrate.solve_bvp(self.ode, self.bc, self.zi, self.sol_init, p=[2000.])
         H = res.p[0]
         z = res.x * H
         y = res.y
--- run_column.py
+++ run_column.py
@@ -7,6 +7,8 @@ N = 4 # number of cases to be considered
 H_max_so = [2000, 2000, 1500, 1500] # depth extent of upwelling in SO
 B_int=[3e3,1.2e4, 3e3, 1.2e4] # abyssal buoyancy loss
 
+psi_so_max=4.
+            
 # Area of the basin (this is for idealized single-basin model)
 a=6.37e6; A = 2*np.pi*a**2*59/360*(np.sin(math.radians(69)) - np.sin(math.radians(-48)))
 
@@ -18,21 +20,21 @@ kappa_4k=3e-4
 kappa = lambda z, H: (kappa_back + kappa_s*np.exp(z*H/100)+kappa_4k*np.exp(-z*H/1000 - 4))  
 dkappa_dz = lambda z, H: (kappa_s/100*np.exp(z*H/100)-kappa_4k/1000*np.exp(-z*H/1000-4))   
 
-
 fig = plt.figure(figsize=(6,10))
 ax1 = fig.add_subplot(111)
-ax2 = ax1.twiny()  
+ax2 = ax1.twiny()
 
 # Main loop which solves column model subject to different parameters:
 for i in range(0, N):
-    m = Model(H_max_so=H_max_so[i], B_int=B_int[i], A=A, kappa=kappa, dkappa_dz=dkappa_dz)
+    psi_so= lambda z, H:  (psi_so_max * 1e6 * np.sin([-np.pi * max(H * x, -H_max_so[i]) / H_max_so[i] for x in z] )**2 )
+    m = Model(B_int=B_int[i], A=A, kappa=kappa, dkappa_dz=dkappa_dz, psi_so=psi_so)
     res = m.solve()
     H = res['H']
-    z = res['z']
+    zg = res['z']
     psi = res['psi']
     print(H)
-    ax1.plot(psi[0,:], z)
-    ax2.plot(psi[2,:], z)
+    ax1.plot(psi[0,:], zg)
+    ax2.plot(psi[2,:], zg)
     plt.ylim((-3e3,0))
     ax1.set_xlim((-5,20))
     ax2.set_xlim((-0.01,0.04))
