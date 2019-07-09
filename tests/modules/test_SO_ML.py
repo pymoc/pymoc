c836d55587a6b1e0ed65807c4624968f9e18ce95
Mike Bueti <mbueti@gmail.com>
2019-07-09 15:37:03 -0400

    Set up so_ml init test and fixtures

--- tests/modules/test_SO_ML.py
+++ tests/modules/test_SO_ML.py
@@ -4,11 +4,40 @@ import numpy as np
 from scipy import integrate
 from pymoc.SO_ML import SO_ML
 
+@pytest.fixture(scope="module", params=[
+  {
+    'y': np.asarray(np.linspace(0, 2.0e6, 51)),
+    'Ks': 100,
+    'h': 50,
+    'L': 4e6,
+    'surflux': 5.9e3,
+    'rest_mask': 0.0,
+    'b_rest': 0.0,
+    'v_pist': 2.0/86400.0,
+    'bs': 0.02
+  },
+  {
+    'y': None,
+  },
+  {
+    'y': 1e6
+  }
+])
+def so_ml_config(request):
+  return request.param
+
 @pytest.fixture(scope="module")
 def so_ml(request):
-  return SO_ML(**{ 'y': np.asarray(np.linspace(-70.0, -55.0, 60)) }) 
+  return SO_ML(y=np.asarray(np.linspace(0, 2.0e6, 51))) 
 
 class TestSO_ML(object):
+  def test_so_ml_init(self, so_ml_config):
+    if not isinstance(so_ml_config['y'], np.ndarray) or not len(so_ml_config['y']):
+      with pytest.raises(TypeError) as yinfo:
+        SO_ML(**so_ml_config)
+      assert(str(yinfo.value) == "y needs to be numpy array providing (regular) grid")
+      return
+
   def test_make_array(self, so_ml):
     myst = np.arange(0.0, 8.0, 0.1)
     assert all(so_ml.make_array(myst, 'myst') == myst)
