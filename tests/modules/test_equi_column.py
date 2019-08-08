8ef90894668ed45b24389ca678c0cfebeee5eb5a
Mike Bueti <mbueti@gmail.com>
2019-08-08 16:38:29 -0400

    begin testing equilibrium column initialization

--- tests/modules/test_equi_column.py
+++ tests/modules/test_equi_column.py
@@ -1,8 +1,71 @@
 import pytest
 import sys
+import inspect
+import numpy as np
+from numpy import testing
 sys.path.append('/pymoc/src/modules')
 from equi_column import Equi_Column
 
+@pytest.fixture(scope="module", params=[
+  {
+    'z': np.asarray(np.linspace(-4000, 0, 80)),
+    'B_int': 3e3,
+    'A': 2.0e14,
+    'kappa': 3e-5,
+    'H': 500.0
+  }
+])
+def column_config(request):
+  return request.param
+
+@pytest.fixture(scope="module")
+def column(request):
+  return Equi_Column(**{
+    'B_int': 3e3,
+    'A': 2.0e14,
+    'kappa': 3e-5
+  })
+
 class TestEqui_Column(object):
-  def test(self):
-    assert 1
+  def test_Equi_Column_init(self, column_config):
+    column_signature = inspect.signature(Equi_Column)
+
+    column = Equi_Column(**column_config)
+
+    for k in ['f', 'A', 'H', 'H_guess']:
+      assert getattr(column, k)  == (column_config[k] if k in column_config and column_config[k] else column_signature.parameters[k].default)
+    for k in ['z']:
+      testing.assert_array_equal(getattr(column, k), (column_config[k] if k in column_config and not column_config[k] is None else column_signature.parameters[k].default))
+
+    nz = column_config['nz'] if 'nz' in column_config and not column_config['nz'] is None else 100
+    for i in range(nz):
+      testing.assert_approx_equal(column.zi[i], i/(nz - 1) - 1)
+
+    assert callable(column.kappa)
+    assert callable(column.dkappa_dz)
+    if 'kappa' in column_config and column_config['kappa']:
+      if callable(column_config['kappa']):
+        print('foo')
+      elif isinstance(column_config['kappa'], np.ndarray):
+        print('bar')
+      else:
+        for z in column_config['z']:
+          assert column.kappa(z, 200.0) == column_config['kappa'] / (4e4*column.f)
+          assert column.dkappa_dz(z, 200.0) == 0
+
+
+
+  def test_alpha(self):
+    return 
+
+  def test_bz(self):
+    return 
+
+  def test_bc(self):
+    return 
+
+  def test_ode(self):
+    return 
+
+  def test_solve(self):
+    return 
