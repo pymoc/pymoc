54bf4b47e7cafb7e1621d326f0eda2be6148e700
Mike Bueti <mbueti@gmail.com>
2019-09-03 07:08:33 -0400

    Begin documentation summary

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'pymoc'
-copyright = '2019, Mike Bueti'
-author = 'Mike Bueti'
+copyright = '2019, Michael Bueti'
+author = 'Michael Bueti'
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,6 +28,7 @@ author = 'Mike Bueti'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -36,7 +37,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
---
+++ docs/index.rst
@@ -0,0 +1,27 @@
+.. pymoc documentation master file, created by
+   sphinx-quickstart on Fri Aug 30 10:54:58 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+PyMOC: Python Meridional Overturning Circulation Model
+======================================================
+
+PyMOC is a simple, modular suite of python ocean column models for
+use in studying the Meridional Overturning Circulation (MOC). The 
+MOC plays a critical role in the uptake and redistribution of heat
+and carbon by the ocean, and as such both mediates and is governed
+by shifts in the climate regime. As such, understanding of the MOC
+is crucial to understanding of climate change.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
