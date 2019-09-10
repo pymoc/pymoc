2566e3dcaa3634e7a6ce91d68a8e45e1bc9094f9
Mike Bueti <mbueti@gmail.com>
2019-09-10 18:37:06 -0400

    switch to jupyter docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
-import sphinx_rtd_theme
+from jupyter_sphinx_theme import *
+init_theme()
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -27,7 +28,7 @@ author = 'Michael Bueti'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx_rtd_theme']
+extensions = ['recommonmark', 'jupyter_sphinx_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -42,7 +43,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'jupyter_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
