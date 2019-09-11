779884c67162566f539af44937b05221e7a45243
Mike Bueti <mbueti@gmail.com>
2019-09-10 20:32:36 -0400

    remove duplicated jupyter extension

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Michael Bueti'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'jupyter_sphinx_theme']
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -43,7 +43,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'jupyter_sphinx_theme'
+html_theme = 'jupyter_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
