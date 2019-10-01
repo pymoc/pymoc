db28300fce6d703691f2063e6e41df449c946990
Mike Bueti <mbueti@gmail.com>
2019-10-01 19:47:06 -0400

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,5 @@ setup(
     name='pymoc',
     version='1.0',
     package_dir={'pymoc': 'src'},
-    py_modules=[
-        'pymoc.modules.column',
-        'pymoc.modules.equi_column',
-        'pymoc.modules.psi_SO',
-        'pymoc.modules.psi_thermwind',
-        'pymoc.modules.SO_ML',
-        'pymoc.utils.check_numpy_version',
-        'pymoc.utils.gridit',
-        'pymoc.utils.make_array',
-        'pymoc.utils.make_func',
-    ]
+    packages=['pymoc', 'pymoc.modules', 'pymoc.utils']
 )
