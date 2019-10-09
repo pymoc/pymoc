d33b234f60ffcc853a6775b547e2e3a1e495e314
Mike Bueti <mbueti@gmail.com>
2019-10-09 19:17:01 -0400

    Add symlink to pymoc for develop mode

--- setup.py
+++ setup.py
@@ -16,6 +16,5 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    ]
 )
