6b3907a3fb3d7a3d508fa2702767e4a730258496
Mike Bueti <mbueti@gmail.com>
2020-02-28 15:53:08 -0500

    Stub out model interface tests

---
+++ tests/modules/test_module_wrapper.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import funcsigs
+import numpy as np
+from scipy import integrate
+import pytest
+from pymoc.utils import make_func, make_array
+sys.path.append('/pymoc/src/pymoc/modules')
+from pymoc.modules import ModuleWrapper
+
+class TestModuleWrapper(object):
+  def test_module_wrapper_init(self):
+
+  def test_module_type(self):
+
+  def test_timestep_basin(self):
+  
+  def test_update_coupler(self):
+
+  def test_b(self):
+
+  def test_validate_neighbors(self):
+
+  def test_backlink_neighbor_lins(self):
---
+++ tests/test_model.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import funcsigs
+import numpy as np
+from scipy import integrate
+import pytest
+from pymoc.utils import make_func, make_array
+sys.path.append('/pymoc/src/pymoc/modules')
+from pymoc import Model
+
+class TestModel(object):
+  def test_model_init(self):
+
+  def test_keys(self):
+
+  def test_get_module(self):
+  
+  def test_validate_neighbors_input(self):
+
+  def test_add_module(self):
+
+  def test_new_module(self):
+
+  def test_run(self):
+
+  def test_timestep(self):
+
+  def test_snapshot(self):
