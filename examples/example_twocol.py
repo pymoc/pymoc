f60f08334b5954f27c0279a25533030820446920
Malte Jansen <mfj@uchicago.edu>
2021-01-20 14:17:06 -0600

    Update example_twocol.py

--- examples/example_twocol.py
+++ examples/example_twocol.py
@@ -38,23 +38,17 @@ total_iters = int(
 kappa_back = 1e-5
 kappa_4k = 3e-4
 
-
 def kappa(z):
   return (kappa_back + kappa_4k * np.exp(-z / 1000 - 4))
-
 
 # create grid:
 z = np.asarray(np.linspace(-4000, 0, 80))
 
-
-# Initial conditions for buoyancy profile in the basin:
+# Initial conditions for buoyancy profile in the basin and north:
 def b_basin(z):
   return bs * np.exp(z / 300.)
-
-
 def b_north(z):
   return 1e-3 * bs * np.exp(z / 300.)
-
 
 # create overturning model instance
 AMOC = Psi_Thermwind(z=z, b1=b_basin, b2=b_north)
